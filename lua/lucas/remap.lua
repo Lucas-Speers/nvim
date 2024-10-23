0910d671c3976c0929f39b49f1f98683c53dda08
Lucas Speers <lucas_speers@outlook.com>
2024-10-23 10:53:52 -0600

    small changes

--- lua/lucas/remap.lua
+++ lua/lucas/remap.lua
@@ -2,6 +2,7 @@ vim.g.mapleader = " "
 vim.keymap.set("n", "<leader>w", vim.cmd.w)
 vim.keymap.set("n", "<leader>s", function() vim.cmd.w() vim.cmd.Ex() end)
 vim.keymap.set("n", "<leader>f", vim.cmd.Ex)
+vim.keymap.set("n", "<leader>q", vim.cmd.q)
 vim.keymap.set("n", "<leader>x", function() vim.cmd("q!") end)
 
 vim.keymap.set("n", "<leader>d", vim.lsp.buf.declaration) 
